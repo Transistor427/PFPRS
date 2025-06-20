5aaa17b80cdfa7c02001e29df33eea683345baff
Vlad <427departament@gmail.com>
2025-06-20 20:34:48 +0300

    Обновлена логика работы скрипта обработки g-кода Изменена логика ожидания нагрева хотенда без стабилизации на 200 градусах на достижение 200 градусов

--- curr_layer.sh
+++ curr_layer.sh
@@ -3,7 +3,14 @@ SD_PATH=~/printer_data/gcodes/
 FILE_NAME=restore.gcode
 
 cat ${2} > /tmp/tempF.$$
-cat /tmp/tempF.$$ | sed -e '1,/;Z:'${1}'/ d' | > ${SD_PATH}/${FILE_NAME}
-echo '_START_PRINT_RESTORE' >> ${SD_PATH}/${FILE_NAME}
-tac /tmp/tempF.$$ | sed -e '/ Z'${1}'[^0-9]*$/q' | tac >> ${SD_PATH}/${FILE_NAME}   
-rm /tmp/tempF.$$
+if [sed -e '1,/;Z:'${1}'/ d']; then
+    cat /tmp/tempF.$$ | sed -e '1,/;Z:'${1}'/ d' | > ${SD_PATH}/${FILE_NAME}
+    echo '_START_PRINT_RESTORE' >> ${SD_PATH}/${FILE_NAME}
+    tac /tmp/tempF.$$ | sed -e '/ Z'${1}'[^0-9]*$/q' | tac >> ${SD_PATH}/${FILE_NAME}   
+    rm /tmp/tempF.$$
+else
+    cat /tmp/tempF.$$ | sed -e '1,/;Z:'${1}'/ d' | > ${SD_PATH}/${FILE_NAME}
+    echo '_START_PRINT_RESTORE' >> ${SD_PATH}/${FILE_NAME}
+    tac /tmp/tempF.$$ | sed -e '/ Z'${1}'[^0-9]*$/q' | tac | tail -n+2 | sed -ne '/ Z/,$ p' >> ${SD_PATH}/${FILE_NAME}
+    rm /tmp/tempF.$$
+fi
